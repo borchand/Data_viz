b62eb2ce1723b3037f26fa96a13326d7cdd5dd85
nicra <nicra@itu.dk>
2023-05-11 22:49:14 +0200

    showing red and yellow flag

--- utils.py
+++ utils.py
@@ -6,6 +6,8 @@ import pandas as pd
 from datetime import timedelta, datetime
 
 TRACK_DISTANCE = 4.309
+MAG_QUALIFYING_LAP = 25
+VER_QUALIFYING_LAP = 22
 
 def IsTurn(x,y,x_last_turn,y_last_turn, turn_difference=1300):
     if x + turn_difference < x_last_turn:
@@ -91,3 +93,13 @@ def convert_delta_to_time(delta):
     datetime_0 = datetime(1900,1,1,0,0,0)
 
     return (datetime_0 + delta)
+
+def get_qualifying_laps_for_mag_and_ver(qualifying_session):
+
+    fast_ver = qualifying_session.pick_driver("VER")
+    fast_mag = qualifying_session.pick_driver("MAG")
+
+    best_lap_mag = fast_mag.pick_lap(MAG_QUALIFYING_LAP).get_car_data()
+    best_lap_ver = fast_ver.pick_lap(VER_QUALIFYING_LAP).get_car_data()
+
+    return best_lap_mag, best_lap_ver
